dbf1fcbaeea71938007b3e18683df530064c4196
Isabela Castilho <icastilhog@gmail.com>
2020-04-22 10:13:06 -0300

    Desafio finalizado

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -9,7 +9,7 @@ export const Container = styled.div<ContainerProps>`
   padding: 30px 0;
 
   header {
-    width: 1120px;
+    max-width: 1120px;
     margin: 0 auto;
     padding: ${({ size }) => (size === 'small' ? '0 20px ' : '0 20px 150px')};
     display: flex;
@@ -21,14 +21,30 @@ export const Container = styled.div<ContainerProps>`
         color: #fff;
         text-decoration: none;
         font-size: 16px;
+        opacity: 0.6;
         transition: opacity 0.2s;
+        position: relative;
 
         & + a {
           margin-left: 32px;
         }
 
         &:hover {
-          opacity: 0.6;
+          opacity: 1;
+        }
+
+        &.active {
+          opacity: 1;
+        }
+
+        &.active:after {
+          content: '';
+          position: absolute;
+          width: 73px;
+          height: 2px;
+          left: 0;
+          top: 27px;
+          background: #ff872c;
         }
       }
     }
